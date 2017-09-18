c1d48ce09fe7e2e565b462229e1ac6ab13bab592
pablo fuentes <pablofuentesg1@yahoo.es>
2017-09-18 13:18:25 +0200

    cambiado fb

--- passport/facebook.js
+++ passport/facebook.js
@@ -3,8 +3,8 @@ const User = require('../models/User');
 const FbStrategy = require('passport-facebook').Strategy;
 
 passport.use(new FbStrategy({
-  clientID: "138265856787332",
-  clientSecret: "f6fa6a3f54ccb85d8c995845279104b5",
+  clientID: "114489382627653",
+  clientSecret: "805ad14643ebf2dc625e0b4a81429894",
   callbackURL: "/auth/facebook/callback"
 }, (accessToken, refreshToken, profile, next) => {
   console.log(profile);
