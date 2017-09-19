bfb5e2f735b0dc4b82b5a89441778738541a5e74
Christian Estepa <chrisestepa@MacBook-Air-de-Christian.local>
2017-09-19 09:57:03 +0200

    controllers ok

---
+++ controllers/dashboardController.js
@@ -0,0 +1,46 @@
+const Match = require("../models/match");
+const path = require('path');
+const debug = require('debug')("app:auth:local");
+
+module.exports = {
+  dashboard: (req, res, next) => {
+    res.render('dashboard/dashboard', {
+      user: req.user
+    });},
+
+  new_dashboard: (req, res) => {
+    res.render('dashboard/new', {
+      user: req.user
+    });},
+
+  new_dashboard_post: (req, res) => {
+    const date = req.body.date;
+    const sport = req.body.sport;
+    const playersNumber = req.body.maxnum;
+
+    if (date === "" || sport === "" || playersNumber === 0) {
+      res.render("dashboard/new", {
+        message: "All fields required"
+      });
+      return;
+    }
+
+    debug("Match created");
+
+    const newMatch = new Match({
+        date,
+        sport,
+        playersNumber
+      })
+      .save()
+      .then(match => res.redirect('/dashboard'))
+      .catch(e => res.render("dashboard/new", {
+        message: "Something went wrong"
+      }));
+    },
+
+  dashboards: (req, res) => {
+    res.render('dashboard/view', {
+      user: req.user
+    });}
+};
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,55 +1,16 @@
 const Match = require("../models/match");
 const path = require('path');
 const debug = require('debug')("app:auth:local");
-
+const dashboardController = require ("../controllers/dashboardController");
 const router = require('express').Router();
 
-router.get('/dashboard', (req, res) => {
-  res.render('dashboard/dashboard', {
-    user: req.user
-  });
-});
+router.get('/dashboard', dashboardController.dashboard);
 
-router.get('/dashboard/new', (req, res) => {
-  res.render('dashboard/new', {
-    user: req.user
-  });
-});
+router.get('/dashboard/new', dashboardController.new_dashboard);
 
-router.post('/dashboard/new', (req, res) => {
-  const date = req.body.date;
-  const sport = req.body.sport;
-  const playersNumber = req.body.maxnum;
+router.post('/dashboard/new', dashboardController.new_dashboard_post);
 
-  if (date === "" || sport === "" || playersNumber === 0) {
-    res.render("dashboard/new", {
-      message: "All fields required"
-    });
-    return;
-  }
-
-  debug("Match created");
-
-  const newMatch = new Match({
-      date,
-      sport,
-      playersNumber
-    })
-    .save()
-    .then(match => res.redirect('/dashboard'))
-    .catch(e => res.render("dashboard/new", {
-      message: "Something went wrong"
-    }));
-
-});
-
-router.get('/dashboard/view', (req, res) => {
-  res.render('dashboard/view', {
-    user: req.user
-  });
-});
-
-
+router.get('/dashboard/view', dashboardController.dashboards);
 
 
 module.exports = router;
