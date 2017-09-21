e6b4dcb428454ef826f59cb3855be6abe5e887b9
Christian Estepa <chrisestepa@MacBook-Air-de-Christian.local>
2017-09-21 14:04:23 +0200

    obtenemos lat y long

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -4,50 +4,55 @@ function initMap() {
       center: {lat: 40.3930556, lng: -3.5430555555555556},
       zoom: 13
     });
-  
+
     var input = /** @type {!HTMLInputElement} */(
         document.getElementById('pac-input'));
-  
+
     var types = document.getElementById('type-selector');
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
-  
+
     var autocomplete = new google.maps.places.Autocomplete(input);
     autocomplete.bindTo('bounds', map);
-  
+
     var infowindow = new google.maps.InfoWindow();
     var marker = new google.maps.Marker({
       map: map,
       anchorPoint: new google.maps.Point(0, -29)
     });
-  
+
     autocomplete.addListener('place_changed', function() {
       infowindow.close();
-      marker.setVisible(false);
+      marker.setVisible(true);
       var place = autocomplete.getPlace();
       if (!place.geometry) {
         window.alert("No details available for input: '" + place.name + "'");
         return;
       }
-  
+
+
+
       if (place.geometry.viewport) {
         map.fitBounds(place.geometry.viewport);
       } else {
         map.setCenter(place.geometry.location);
+          console.log(place.geometry.location);
         map.setZoom(17);
       }
       marker.setIcon(/** @type {google.maps.Icon} */({
-        url: place.icon,
+        url: "/avatars/marker.png",
         size: new google.maps.Size(71, 71),
         origin: new google.maps.Point(0, 0),
         anchor: new google.maps.Point(17, 34),
         scaledSize: new google.maps.Size(35, 35)
       }));
       marker.setPosition(place.geometry.location);
-      document.getElementById('lat').value = place.geometry.location.lat();
-      document.getElementById('lon').value = place.geometry.location.lng();
+      // document.getElementById('lat').value = place.geometry.location.lat();
+      // document.getElementById('lon').value = place.geometry.location.lng();
+      lat = place.geometry.location.lat();
+      long = place.geometry.location.lng();
       marker.setVisible(true);
-  
+
       var address = '';
       if (place.address_components) {
         address = [
@@ -56,9 +61,13 @@ function initMap() {
           (place.address_components[2] && place.address_components[2].short_name || '')
         ].join(' ');
       }
-  
+
       infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
       infowindow.open(map, marker);
+
+      console.log("LAT: " + lat);
+      console.log("LONG: " + long);
     });
+
   }
   initMap();
